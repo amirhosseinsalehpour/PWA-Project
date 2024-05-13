40d529b044e2334fc0647a3ed722e0a96aa1eb96
AmirHossein Salehpour <salehpouramirhossein19@gmail.com>
2024-05-13 13:45:25 +0330

    delete old cache

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,11 @@
+var CACHE_STATIC_NAME = "static-v1";
+var CACHE_DYNAMIC_NAME = "dynamic-v1";
+
 self.addEventListener("install", function (event) {
   console.log("installing service worker....", event);
   //   Because I want it to wait until it is created and then go to the next one => "waitUntil"
   event.waitUntil(
-    caches.open("static").then(function (cache) {
+    caches.open(CACHE_STATIC_NAME).then(function (cache) {
       console.log("Pre caching App shell", cache);
       cache.addAll([
         "/",
@@ -23,6 +26,18 @@ self.addEventListener("install", function (event) {
 });
 
 self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map(function (key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log("Service Worker removing cache", key);
+            caches.delete(key);
+          }
+        })
+      );
+    })
+  );
   console.log("activating service worker....", event);
   return self.clients.claim();
 });
@@ -33,12 +48,14 @@ self.addEventListener("fetch", function (event) {
       if (response) {
         return response;
       } else {
-        return fetch(event.request).then(function (res) {
-          return caches.open("dynamic").then(function (cache) {
-            cache.put(event.request.url, res.clone());
-            return res;
-          });
-        });
+        return fetch(event.request)
+          .then(function (res) {
+            return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+              cache.put(event.request.url, res.clone());
+              return res;
+            });
+          })
+          .catch(function () {});
       }
     })
   );
